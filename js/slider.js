674adb7eb7c60eccc4915e71405bbc456eab4a4a
Thanh Phan <th.phan1408@gmail.com>
2021-04-22 13:18:35 +0700

    first commit

---
+++ js/slider.js
@@ -0,0 +1,59 @@
+/*
+    -----------------------
+    -----------------------
+       Glide Js Carousel
+    -----------------------
+    -----------------------
+*/
+const slider1 = document.getElementById("glide_1");
+const slider2 = document.getElementById("glide_2");
+/*
+    -----------------------
+    -----------------------
+            Hero
+    -----------------------
+    -----------------------
+*/
+if(slider1){
+    new Glide(slider1, {
+        type: "carousel", 
+        startAt: 0,
+        // autoplay: 3000,
+        hoverpause: true,
+        perView: 1,
+        animationDuration: 800,
+        animationTimingFunc: "linear"
+    }).mount();
+}
+/*
+    -----------------------
+    -----------------------
+        Latest Products
+    -----------------------
+    -----------------------
+*/
+if(slider2){
+    new Glide(slider2, {
+        type: "carousel", 
+        startAt: 0,
+        hoverpause: true,
+        perView: 4,
+        animationDuration: 800,
+        animationTimingFunc: "ease-in-out",
+        breakpoints:{
+            1200:{
+                perView: 3,
+            },
+            768:{
+                perView: 2,
+            },
+        }
+    }).mount();
+}
+/*
+    -----------------------
+    -----------------------
+        Testimoial
+    -----------------------
+    -----------------------
+*/
